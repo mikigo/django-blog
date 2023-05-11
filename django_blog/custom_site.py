7c9dc2c2975bc1de99ea8ad00baae499b7d1707d
mikigo <huangmingqiang@uniontech.com>
2023-05-11 15:18:23 +0800

    uupdate

--- django_blog/custom_site.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.admin import AdminSite
-
-class CustomSite(AdminSite):
-    site_header = "django_blog"
-    site_title = "django_blog 管理后台"
-    index_title = "django_blog"
-
-custom_site = CustomSite(name="cus_admin")
