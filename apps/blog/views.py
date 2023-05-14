33727583d7218d90c94310de4e5282b4b55b3348
mikigo <huangmingqiang@uniontech.com>
2023-05-14 16:56:02 +0800

    update

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,50 +1,114 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 from apps.blog.models import Tag, Post, Category
 from apps.config.models import SideBar
 
 
-def post_list(request, category_id=None, tag_id=None):
-    tag = None
-    category = None
+# def post_list(request, category_id=None, tag_id=None):
+#     tag = None
+#     category = None
+#
+#     if tag_id:
+#         post_list, tag = Post.get_by_tag(tag_id)
+#     elif category_id:
+#         post_list, category = Post.get_by_category(category_id)
+#     else:
+#         post_list = Post.latest_posts()
+#
+#     context = {
+#         "category": category,
+#         "tag": tag,
+#         "post_list": post_list,
+#         "sidebars": SideBar.get_all(),
+#     }
+#     # 把所有分类加进来
+#     context.update(Category.get_navs())
+#
+#     return render(
+#         request,
+#         "blog/list.html",
+#         context=context
+#     )
+#
+#
+# def post_detail(request, post_id):
+#     try:
+#         post = Post.objects.get(id=post_id)
+#     except Post.DoesNotExist:
+#         post = None
+#
+#     context = {
+#         "post": post,
+#         "sidebars": SideBar.get_all(),
+#     }
+#     return render(
+#         request,
+#         "blog/detail.html",
+#         context=context
+#     )
 
-    if tag_id:
-        post_list, tag = Post.get_by_tag(tag_id)
-    elif category_id:
-        post_list, category = Post.get_by_category(category_id)
-    else:
-        post_list = Post.latest_posts()
 
-    context = {
-        "category": category,
-        "tag": tag,
-        "post_list": post_list,
-        "sidebars": SideBar.get_all(),
-    }
-    # 把所有分类加进来
-    context.update(Category.get_navs())
+class CommonViewMixin:
 
-    return render(
-        request,
-        "blog/list.html",
-        context=context
-    )
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(
+            {
+                "sidebars": SideBar.get_all()
+            }
+        )
 
 
-def post_detail(request, post_id):
-    try:
-        post = Post.objects.get(id=post_id)
-    except Post.DoesNotExist:
-        post = None
+class IndexView(CommonViewMixin, ListView):
+    queryset = Post.latest_posts()
+    paginate_by = 5
+    context_object_name = "post_list"
+    template_name = "blog/list.html"
 
-    context = {
-        "post": post,
-        "sidebars": SideBar.get_all(),
-    }
-    return render(
-        request,
-        "blog/detail.html",
-        context=context
-    )
+
+class CategoryView(IndexView):
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs.get("category_id")
+        category = get_object_or_404(Category, pk=category_id)
+        context.update(
+            {
+                "category": category
+            }
+        )
+        return context
+
+    def get_queryset(self):
+        """重写queryset，根据分类过滤"""
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get("category_id")
+        return queryset.filter(category_id=category_id)
+
+
+class TagView(IndexView):
+
+    def get_context_data(self, **kwargs):
+        context = super(TagView, self).get_context_data(**kwargs)
+        tag_id = self.kwargs.get("tag_id")
+        tag = get_object_or_404(Tag, pk=tag_id)
+        context.update(
+            {
+                "tag": tag,
+            }
+        )
+        return context
+
+    def get_queryset(self):
+        queryset = super(TagView, self).get_queryset()
+        tag_id = self.kwargs.get("tag_id")
+        return queryset.filter(tag_id=tag_id)
+
+
+class PostDetailView(CommonViewMixin, DetailView):
+    queryset = Post.latest_posts()
+    template_name = "blog/detail.html"
+    context_object_name = "post"
+    pk_url_kwarg = "post_id"
--- django_blog/urls.py
+++ django_blog/urls.py
@@ -16,14 +16,15 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from apps.blog.views import post_list, post_detail
+# from apps.blog.views import post_list, post_detail
 from apps.config.views import links
+from apps.blog.views import IndexView, CategoryView, TagView, PostDetailView
 
 urlpatterns = [
-    url(r"^$", post_list, name="index"),
-    url(r"^category/(?P<category_id>\d+)/$", post_list, name="category-list"),
-    url(r"^tag/(?P<tag_id>\d+)/$", post_list, name="tag-list"),
-    url(r"^post/(?P<post_id>\d+).html$", post_detail, name="post-detail"),
+    url(r"^$", IndexView.as_view(), name="index"),
+    url(r"^category/(?P<category_id>\d+)/$", CategoryView.as_view(), name="category-list"),
+    url(r"^tag/(?P<tag_id>\d+)/$", TagView.as_view(), name="tag-list"),
+    url(r"^post/(?P<post_id>\d+).html$", PostDetailView.as_view(), name="post-detail"),
     url(r"links/$", links, name="links"),
 
     url(r'^admin/', admin.site.urls, name="admin"),
