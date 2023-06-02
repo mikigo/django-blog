88ce345d06af32ff3f54a9984d4077ecf000171c
mikigo <huangmingqiang@uniontech.com>
2023-06-02 19:39:35 +0800

    update

--- apps/blog/serializer.py
+++ apps/blog/serializer.py
@@ -3,12 +3,42 @@ from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
 
+    category = serializers.SlugRelatedField(
+        read_only=True,
+        # 指定要展示的字段
+        slug_field="name",
+    )
+    tag = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field="name",
+    )
+    owner = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field="username",
+    )
+    created_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+
     class Meta:
         model = Post
         fields = [
+            "id",
             "title",
             "category",
-            "desc",
-            "content",
+            "tag",
+            "owner",
             "created_time",
+        ]
+
+class PostDetailSerializer(PostSerializer):
+
+    class Meta:
+        model = Post
+        fields = [
+            "id",
+            "title",
+            "category",
+            "tag",
+            "owner",
+            "content",
         ]
